3ba11b856ae7cb5ef954edd32f29f18e07f74e8f
Julio <julio.mech@hotmail.com>
2020-09-06 17:41:13 -0300

    Teste front-end

---
+++ android/app/src/main/kotlin/com/example/projetozero_libras/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.projetozero_libras
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Home.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+int _valor = 0;
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      /*appBar: AppBar(
+        title: Text(
+          "Algum App",
+          style: TextStyle(color: Colors.blueAccent, fontSize: 20),
+        ),
+        backgroundColor: Colors.white,
+        
+      ),*/
+      body: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: <Widget>[
+          SliverAppBar(
+            stretch: true,
+            onStretchTrigger: () {
+              return;
+            },
+            expandedHeight: 150.0,
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              title: const Text(
+                "Algum App",
+                style: TextStyle(color: Colors.pink),
+              ),
+              background: Stack(
+                fit: StackFit.expand,
+                children: [
+                  const DecoratedBox(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment(0.0, 0.5),
+                        end: Alignment(0.0, 0.0),
+                        colors: <Color>[
+                          Color(0x60001000),
+                          Color(0x00000000),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              //backgroundColor: Colors.blueAccent,
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                ListTile(
+                  leading: Icon(Icons.wb_sunny),
+                  title: Text("Lista item 1"),
+                  subtitle: Text("Numero 1"),
+                ),
+                ListTile(
+                  leading: Icon(Icons.plus_one),
+                  title: Text('Aperta pata +1'),
+                  //subtitle: Text('sunny, h: 80, l: 65'),
+                  onTap: () {
+                    setState(() {
+                      _valor++;
+                    });
+                  },
+                  subtitle: Text("$_valor"),
+                ),
+                ListTile(
+                  leading: Icon(Icons.wb_sunny),
+                  title: Text('Monday'),
+                  subtitle: Text('sunny, h: 80, l: 65'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.wb_sunny),
+                  title: Text('Monday'),
+                  subtitle: Text('sunny, h: 80, l: 65'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.wb_sunny),
+                  title: Text('Monday'),
+                  subtitle: Text('sunny, h: 80, l: 65'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.wb_sunny),
+                  title: Text('Monday'),
+                  subtitle: Text('sunny, h: 80, l: 65'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.wb_sunny),
+                  title: Text('Monday'),
+                  subtitle: Text('sunny, h: 80, l: 65'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.wb_sunny),
+                  title: Text('Monday'),
+                  subtitle: Text('sunny, h: 80, l: 65'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.wb_sunny),
+                  title: Text('Monday'),
+                  subtitle: Text('sunny, h: 80, l: 65'),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
